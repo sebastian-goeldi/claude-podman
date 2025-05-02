326489dc155ece3f8e04ebb0e6ed42e682140496
Evan Carroll <me@evancarroll.com>
2025-05-01 20:56:38 -0500

    Initial commit

---
+++ devops/build-image.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+CONTAINER=$(buildah from docker.io/node:current-alpine)
+CLAUDE_VERSION=$(npm info @anthropic-ai/claude-code --json | jq .version -r)
+IMAGE=claude-code
+
+#apk add --no-cache dash
+buildah run "$CONTAINER" sh <<EOT
+	npm config set os linux
+	apk add --no-cache zsh
+	npm --os=linux install --omit=dev --no-audit --no-fund -g @anthropic-ai/claude-code
+	apk cache clean
+	rm -rf /usr/local/lib/node_modules/npm/man/
+	find . -type f -name '*.md' -delete 2> /dev/null
+	adduser -D claude
+EOT
+
+buildah config \
+	--author "Evan Carroll" \
+	--env "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" \
+	--env "SHELL=/usr/bin/zsh" \
+	--cmd "" \
+	--entrypoint '[ "/usr/local/bin/node", "--no-warnings", "--enable-source-maps", "/usr/local/bin/claude" ]' \
+	"$CONTAINER"
+
+buildah commit \
+	--rm \
+	"$CONTAINER" "$IMAGE"
+
+buildah tag "$IMAGE" "$CLAUDE_VERSION"
+
+echo To use the image [${IMAGE}:${CLAUDE_VERSION}] run /bin/claude
